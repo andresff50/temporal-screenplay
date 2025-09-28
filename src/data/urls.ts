0badc8878baa219d4aa1545510014b36195818f5
andres <andresvz50@gmail.com>
2025-09-27 22:35:25 -0500

    Update urls.ts

--- src/data/urls.ts
+++ src/data/urls.ts
@@ -1,5 +1,5 @@
 export function getSalesforceUrl(ambiente: string): string {
   if (!ambiente) return '';
   const env = ambiente.toLowerCase();
-  return `https://clarosfi--${env}.sandbox.my.salesforce.com/`;
+  return `https://c----------${env}.---------`;
 }
